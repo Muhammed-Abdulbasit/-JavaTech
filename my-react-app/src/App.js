4e1ead184442a51506cf415b672a4066f63226d1
programmersyed <abrahamsyed10@gmail.com>
2023-11-14 13:10:13 -0500

    Github repo UI

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -14,13 +14,17 @@ const App = () => {
       {/* Display SearchScreen and pass the handleSearch function */}
       <SearchScreen onSearch={handleSearch} />
 
-      {/* Display search results */}
+      {/* Display search results in a GitHub repository format */}
       <div style={styles.resultsContainer}>
         <h2 style={styles.resultsTitle}>Search Results:</h2>
         {searchResults.map((result, index) => (
-          <p key={index} style={styles.resultText}>
-            {result}
-          </p>
+          <div key={index} style={styles.repositoryCard}>
+            <h3 style={styles.repositoryName}>{result}</h3>
+            <p style={styles.repositoryDescription}>
+              This is a sample description for the repository.
+            </p>
+            <button style={styles.viewButton}>View Repository</button>
+          </div>
         ))}
       </div>
     </div>
@@ -45,10 +49,31 @@ const styles = {
     marginBottom: 8,
     color: '#333',
   },
-  resultText: {
+  repositoryCard: {
+    border: '1px solid #ddd',
+    borderRadius: '8px',
+    padding: '16px',
+    margin: '16px',
+    textAlign: 'left',
+  },
+  repositoryName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: '8px',
+    color: '#333',
+  },
+  repositoryDescription: {
     fontSize: 16,
-    marginBottom: 4,
+    marginBottom: '8px',
     color: '#555',
+  },
+  viewButton: {
+    backgroundColor: '#007BFF',
+    color: '#fff',
+    padding: '8px 16px',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
   },
 };
 
