b761272c0e746b94c9dc8eac1b9f006a75afdb9d
Muhammed-Abdulbasit <muhammedabdulbasit2654@gmail.com>
2023-11-26 18:05:46 -0500

    Home page updated
    Added drop down

--- GitHubGo/src/HomeScreen.js
+++ GitHubGo/src/HomeScreen.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate} from 'react-router-dom';
+import './HomeScreen.css';
+import ProfileDropdown from './ProfileDropdown';
+import profileIcon from './JavaTechLogo.png';
+
 
 export const HomeScreen = () => {
  const navigate = useNavigate();
+ const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
+ const toggleDropdown = () => {
+   setIsDropdownOpen(!isDropdownOpen);
+ };
 
  return (
    <div>
-     <h1>HomeScreen</h1>
-     <button onClick={()=> {navigate("/SearchScreen");}}>Search</button>
+    <div className='container'>
+     <h1 className='title'>GitHub Go</h1>
+     <button className ="searchScreenButton" onClick={()=> {navigate("/SearchScreen");}}>Search</button>
+     <h2 className='previous-searches' > Previous Searches:</h2>
+     </div>
+
+     <div className='profile-icon' onClick={toggleDropdown}>
+        {/* Your profile icon goes here */}
+        <img src={profileIcon} alt='Profile Icon' />
+        {isDropdownOpen && <ProfileDropdown />}
+    </div>
    </div>
+   
  );
 };
