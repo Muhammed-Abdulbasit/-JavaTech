ffc7f485b456a7244e062084e452e630b8a4ef2b
tyman2323 <122325523+tyman2323@users.noreply.github.com>
2023-11-29 11:53:12 -0500

    Update Login.js

--- GitHubGo/src/Login.js
+++ GitHubGo/src/Login.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
-import { useEffect } from 'react';
+
 export const Login = () => {
   const [formData, setFormData] = useState({
     username: '',
@@ -10,6 +10,7 @@ export const Login = () => {
 
   const navigate = useNavigate();
   const CLIENT_ID = 'e43509bd8930071520ba';
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -18,11 +19,12 @@ export const Login = () => {
     }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // Add authentication logic here (e.g., API call, validation, etc.)
-    // Redirect to the home page after successful login
-    navigate('/');
+    // For example, you might want to send formData to your backend server for authentication
+    // If authentication is successful, redirect to the home page
+    // navigate('/');
   };
 
   useEffect(() => {
@@ -30,50 +32,45 @@ export const Login = () => {
     const urlpara = new URLSearchParams(querystring);
     const codeparam = urlpara.get('code');
     console.log(codeparam);
+    // You might want to send this codeparam to your backend server to exchange for an access token
   }, []);
 
-  function loginwithgithub(){
-    window.location.assign("https://github.com/login/oauth/authorize?client_id="+CLIENT_ID);
-  }
+  const loginWithGithub = () => {
+    window.location.assign(`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`);
+  };
+
   return (
-  <div className="login-container">
-    <div className="title">
-      <h1>GitHub Go</h1>
-    </div>
+    <div className="login-container">
+      <div className="title">
+        <h1>GitHub Go</h1>
+      </div>
       <h2 className="second-title">Login</h2>
       <form onSubmit={handleSubmit}>
-  <div>
-    <label htmlFor="username">Username: </label>
-    <input
-      type="text"
-      id="username"
-      name="username"
-      value={formData.username}
-      onChange={handleChange}
-      required
-    />
-  </div>
-
-  <div>
-    <label htmlFor="password">Password: </label>
-    <input
-      type="password"
-      id="password"
-      name="password"
-      value={formData.password}
-      onChange={handleChange}
-      required
-    />
-  </div>
-
-  <button onClick={loginwithgithub}>Login</button>
-</form>
-
-      <p>
-        Please login with your GitHub username and password
-      </p>
-      
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="button" onClick={loginWithGithub}>Login with GitHub</button>
+      </form>
+      <p>Please login with your GitHub username and password</p>
     </div>
   );
 };
-
