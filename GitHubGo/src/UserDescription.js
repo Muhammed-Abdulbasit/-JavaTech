fd2515b4f78efa4ccd04703866e096858642737a
Muhammed-Abdulbasit <muhammedabdulbasit2654@gmail.com>
2023-11-30 12:59:52 -0500

    Fixed text color issue

--- GitHubGo/src/UserDescription.js
+++ GitHubGo/src/UserDescription.js
@@ -13,17 +13,17 @@ export const UserDescription = () => {
           <div className="leftContainer">
           <div className="programmerCard">
             <h2>User Description</h2>
-            <p>
+            <p className="userText">
               Programmersyed is a senior developer at Microsoft with years of experience in software development. He specializes in creating innovative solutions and is passionate about coding, problem-solving, and staying up-to-date with the latest technologies.
             </p>
           </div>
           <div className="activityCard">
             <h2>User Activity</h2>
-            <p>
+            <p className="userText">
               Programmersyed committed 36 lines of code to Bing. Check out their recent activity on GitHub.
             </p>
             <hr />
-            <p>
+            <p className="userText">
               In their latest commit, Programmersyed made significant contributions, committing 100 lines of code to another repository.
             </p>
             </div>
