09b53687d31ced3f0f97e7094b24f58581d6b345
programmersyed <abrahamsyed10@gmail.com>
2023-11-30 14:16:50 -0500

    USerDescription

--- GitHubGo/src/UserDescription.js
+++ GitHubGo/src/UserDescription.js
@@ -1,36 +1,58 @@
 // UserDescription.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header } from './Header';
-import './UserDescription.css'; 
+import './UserDescription.css';
 
 export const UserDescription = () => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch('https://api.github.com/users/Muhammed-Abdulbasit');
+        if (response.ok) {
+          const data = await response.json();
+          setUserData(data);
+        } else {
+          console.error('Failed to fetch user data:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchUserData();
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="userContainer">
-        <h1 className="title">Username</h1>
+        <h1 className="title">{userData ? userData.name : 'Loading...'}</h1>
         <div className="cardContainer">
           <div className="leftContainer">
-          <div className="programmerCard">
-            <h2>User Description</h2>
-            <p className="userText">
-              Programmersyed is a senior developer at Microsoft with years of experience in software development. He specializes in creating innovative solutions and is passionate about coding, problem-solving, and staying up-to-date with the latest technologies.
-            </p>
-          </div>
-          <div className="activityCard">
-            <h2>User Activity</h2>
-            <p className="userText">
-              Programmersyed committed 36 lines of code to Bing. Check out their recent activity on GitHub.
-            </p>
-            <hr />
-            <p className="userText">
-              In their latest commit, Programmersyed made significant contributions, committing 100 lines of code to another repository.
-            </p>
-            </div>
+            {userData && (
+              <>
+                <div className="programmerCard">
+                  <h2>User Description</h2>
+                  <p className="userText">{userData.bio || 'No description available'}</p>
+                </div>
+                <div className="activityCard">
+                  <h2>User Activity</h2>
+                  <p className="userText">
+                    {`${userData.name} committed ${userData.public_repos} lines of code to ${userData.company || 'No company'}. Check out their recent activity on GitHub.`}
+                  </p>
+                  <hr />
+                  <p className="userText">
+                    {`In their latest commit, ${userData.name} made significant contributions, committing ${userData.public_gists} lines of code to another repository.`}
+                  </p>
+                </div>
+              </>
+            )}
           </div>
           <div className="rightContainer">
-              <h2>Profile options</h2>
-              <ul className="profileOptionsList">
+            <h2>Profile options</h2>
+            <ul className="profileOptionsList">
               <li>Edit Profile</li>
               <li>Change Password</li>
               <li>Privacy Settings</li>
@@ -46,3 +68,4 @@ export const UserDescription = () => {
     </div>
   );
 };
+
